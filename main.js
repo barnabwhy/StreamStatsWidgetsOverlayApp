37f72f0b209e53bcb1d543b6e4f361619eb60d5b
Barnaby <barnab.why@gmail.com>
2021-10-03 11:47:04 +0100

    Add auto update logic

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, autoUpdater, dialog } = require('electron')
+
+// Auto updater
+const server = 'https://stream-stats-widgets-overlay-app-update-server.vercel.app/'
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
+
+autoUpdater.setFeedURL({ url })
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000)
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', message => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
+
 const path = require('path')
 const fs = require('fs')
 var basepath = app.getAppPath();
