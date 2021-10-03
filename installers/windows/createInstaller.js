abbe5d4be2d796683755d0425a0980d6784fdfe3
Barnaby <barnab.why@gmail.com>
2021-10-03 16:12:30 +0100

    v1.1.3

--- installers/windows/createInstaller.js
+++ installers/windows/createInstaller.js
@@ -9,7 +9,7 @@ getInstallerConfig()
   })
 
 function getInstallerConfig () {
-  console.log('creating windows installer')
+  console.log('Creating Windows installer')
   const rootPath = path.join('./')
   const outPath = path.join(rootPath, 'release-builds')
 
